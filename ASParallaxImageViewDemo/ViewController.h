4ad8c354d2f01bfef2e8e1973a1b535bcf0974a5
Aled Samuel <aledshim@hotmail.com>
2015-10-17 16:31:13 +0100

    Initial Commit

---
+++ ASParallaxImageViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASParallaxImageViewDemo
+//
+//  Created by Aled Samuel on 17/10/2015.
+//  Copyright © 2015 Aled Samuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ASParallaxImageViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ASParallaxImageViewDemo
+//
+//  Created by Aled Samuel on 17/10/2015.
+//  Copyright © 2015 Aled Samuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
